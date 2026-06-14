1445f1b378ec55c86f001d5f717310b7eba5e663
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-14 17:48:24 +0400

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running!');
+});
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) =>{
+    // console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+})
+
+app.listen(5000, () => {
+    console.log("Server started on PORT 5000");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server started on PORT ${PORT}`));
+
 
